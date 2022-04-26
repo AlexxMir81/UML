36a269e4c4efa44783343ae43d151135c4beccc3
alexxmir81 <_alexx@mail.ru>
2022-04-26 17:51:43 +0300

    day1

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -1,6 +1,8 @@
 #include<windows.h>
 #include"resource.h"
 
+CONST CHAR* string[] = {"this", "is", "my", "first", "list", "box", "1024", "256"};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -14,8 +16,20 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
+	{
+		HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; sizeof(string) / sizeof(string[0]); i++)
+		{
+		SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)string[i]);
+		}
+	}
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:break;
+		case IDCANCEL: EndDialog(hwnd, 0);break;
+		}
 		break;
 	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
